133658a1b785765e103949480369c6a94f0bf09f
Patryk Ordon <p.ordon.po@gmail.com>
2022-12-16 22:37:58 +0100

    ok
    menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Header from './components/Header/Header';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home isLoggedIn={loggedIn} />}></Route>
         <Route path="/sign-up" element={<Register isLoggedIn={loggedIn}/>}></Route>
