049a888e87e429e68840b7f75a5ce2141a47ad51
Marti-code <marti.huza@gmail.com>
2022-12-16 21:33:59 +0100

    room
    room

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './components/common/sass/App.scss';
-import { useIsLoggedIn } from './hooks/useIsLoggedIn';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
+import React from "react";
+import logo from "./logo.svg";
+import "./components/common/sass/App.scss";
+import { useIsLoggedIn } from "./hooks/useIsLoggedIn";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Main from "./components/Room/Main";
 
 function App() {
-  
-  const { loggedIn, setLoggedIn, userData, setUserData, loading, getData} = useIsLoggedIn();
+  const { loggedIn, setLoggedIn, userData, setUserData, loading, getData } =
+    useIsLoggedIn();
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home isLoggedIn={loggedIn} />}></Route>
-        <Route path="/sign-up" element={<Register isLoggedIn={loggedIn}/>}></Route>
-        <Route path="/sign-in" element={<Login isLoggedIn={loggedIn} setLoggedIn={setLoggedIn} setUserData={setUserData} />}></Route>
+        <Route
+          path="/sign-up"
+          element={<Register isLoggedIn={loggedIn} />}
+        ></Route>
+        <Route
+          path="/sign-in"
+          element={
+            <Login
+              isLoggedIn={loggedIn}
+              setLoggedIn={setLoggedIn}
+              setUserData={setUserData}
+            />
+          }
+        ></Route>
+        <Route path="/test" element={<Main />}></Route>
       </Routes>
     </div>
   );
